2320b0c4e3babb9672eb0f14554d3822c9b35b7f
diii2409 <duynhut366@gmail.com>
2025-09-03 14:57:51 +0700

    romove dotnet

---
+++ DotNet/lap11-main/EmployeeManagement/Services/GetEmployeesResponse.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Services
+{
+    public class GetEmployeesResponse
+    {
+        public List<Employee> Employees { get; internal set; }
+        public int StatusCode { get; internal set; }
+        public string Message { get; internal set; }
+    }
+}
